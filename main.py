a7f09e716f5676703c0ed741960293b0e1281768
MiataBoy <MiataBoyMX@gmail.com>
2022-11-16 00:08:44 +0100

    uhh idk

--- main.py
+++ main.py
@@ -26,28 +26,27 @@ else:
     print("{}Settings file created successfully.".format(Colors.Green))
 
 menu = ""
+while True:
+    while not (menu == "A" or menu == "B" or menu == "C" or menu == "D"):
+        menu = input(f"""
+    {Colors.Magenta}THE DELUSIONIST
+    {Colors.Bright_Green}A game by Fyuki Games
+    
+    {Colors.Green}A) Play Game
+    {Colors.Bright_Yellow}B) Settings (Coming later)
+    {Colors.Yellow}C) Credits
+    {Colors.Red}D) Exit{Colors.Reset}
+    """).upper()
 
-while not (menu == "A" or menu == "B" or menu == "C" or menu == "D"):
-    menu = input(f"""
-{Colors.Magenta}THE DELUSIONIST
-{Colors.Bright_Green}A game by Fyuki Games
+    if menu == "A":
+        from src.game.playthrough import gameplay
 
-{Colors.Green}A) Play Game
-{Colors.Bright_Yellow}B) Settings (Coming later)
-{Colors.Yellow}C) Credits
-{Colors.Red}D) Exit{Colors.Reset}
-""").upper()
+    if menu == "B":
+        from src.settings.set import settingDisplay
+        settingDisplay()
 
-clear()
+    if menu == "C":
+        from src.game import credits
 
-if menu == "A":
-    from src.game.playthrough import gameplay
-
-if menu == "B":
-    from src.settings import set
-
-if menu == "C":
-    from src.game import credits
-
-if menu == "D":
-    exit("Exiting the game for you. Please come again later!")
+    if menu == "D":
+        exit("Exiting the game for you. Please come again later!")
