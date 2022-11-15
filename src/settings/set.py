579752fd43ddd6b820ce70961a507dc848d6088f
MiataBoy <MiataBoyMX@gmail.com>
2022-11-16 00:09:03 +0100

    somewhat fix settings, you cant exit it...?

--- src/settings/set.py
+++ src/settings/set.py
@@ -1,63 +1,43 @@
 from data.utility import TextColors as Colors
 from data.utility import clear
+import json
 
 import os
 
-enter = input(f"""
-{Colors.Red}We are sorry for the inconvenience, but we ran into some issues with the settings menu :(
-{Colors.Reset}We are working hard to resolve this (Check back in our discord sometimes!)
-You can find our discord here: https://discord.gg/wf89Guxdjb
+def settingDisplay():
+    settingChoose = ""
+    setValue = ""
 
-{Colors.Green}For now, we can give you steps to changing the settings through our settings file.{Colors.Reset}
+    while not (settingChoose == 'A' or settingChoose == 'Cancel'):
+        clear()
+        with open('src/settings/settings.json') as fp:
+            settings = json.loads(fp.read())
 
-1) Go to {Colors.Green}file://{os.path.dirname(os.path.abspath(__file__))}/settings.json{Colors.Reset}
-2) Change the desired setting to either "True" or "False". Anything else may break it- or assign it to a specific value.
-3) Save the file
-4) Exit the game
-5) Start the game back up and enjoy!
+        settingChoose = input(f"""
+What setting would you like to change?
+(Type "{Colors.Yellow}Cancel{Colors.Reset}" to cancel)
 
-{Colors.Yellow}Default Configuration:{Colors.Reset}
-{'{'}
-  "flushing": "{Colors.Green}True{Colors.Reset}"
-{'}'}
+A) {Colors.Green if settings['flushing'] == 'True' else Colors.Red}Segmental flushing: {settings['flushing']}{Colors.Reset}""").capitalize()
 
-{Colors.Yellow}What settings do:{Colors.Reset}
-Flushing: Flushing enables clearing of the console before each new segment in the game
-""")
+    if settingChoose == "Cancel":
+        return # should exit function, doesnt
 
-clear()
-exit(os.system('python main.py'))
-#settingChoose = ""
-#settingSetting = ""
-#exiting = False
+    while not (setValue == "True" or setValue == "False" or setValue == "Cancel"):
+        setValue = input(f"Would you like to set this setting to \"{Colors.Green}True{Colors.Reset}\" or \"{Colors.Red}False{Colors.Reset}\"? Type {Colors.Yellow}\"Cancel{Colors.Reset}\" to cancel.").capitalize()
 
-#with open('src/settings/settings.json') as fp:
-#    settings = json.loads(fp.read())
+    if setValue == "Cancel":
+        return # seems to work
 
-#while exiting == False:
-#    while not (settingChoose == "A" or settingChoose == "Exit"):
-#        settingChoose = input(f"""
-#Please choose one of the following settings to edit or type \"exit\" to exit:
-#({Colors.Yellow}Settings can only be changed to {Colors.Green}\"True\" {Colors.Yellow}or {Colors.Red}\"False\"{Colors.Reset})
-
-#{Colors.Blue}A) Console Flushing [Clears console before each new segment]:{Colors.Green if settings["flushing"] == "True" else Colors.Red} {settings["flushing"]}{Colors.Reset}
-#    """).upper()
-
-#    if settingChoose == "A":
-#        while not (settingSetting == "True" or settingSetting == "False" or settingSetting == "Exit"):
-#            settingSetting = input(f"Would you like to set this to {Colors.Green}\"True\" {Colors.Reset}or {Colors.Red}\"False\"{Colors.Reset}?").upper()
-
-#        if settingSetting == "True":
-#            settings["flushing"] = True
-#            break
-#        if settingSetting == "False":
-#            settings["flushing"] = False
-#            break
-#        if settingSetting == "Exit":
-#            break
-
-#    if settingChoose == "Exit":
-#        break
+    elif setValue == "True":
+        changedSetting = {
+            "flushing": setValue
+        }
+    else:
+        changedSetting = {
+            "flushing": setValue
+        }
 
 
-
+    with open('src/settings/settings.json', 'w') as outfile:
+        json.dump(changedSetting, outfile)
+        return # fix this not returning back to the ask which setting to change menu
