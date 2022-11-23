256fd1a6a2a004044a2bab882ad323319786ab6f
DeltaSLM <joshuaslui0203@gmail.com>
2022-11-23 13:36:50 +0100

    fix description

--- src/game/credits.py
+++ src/game/credits.py
@@ -5,15 +5,15 @@ from data.utility import clear
 
 input(f"""
 {Colors.Magenta}The Delusionist{Colors.Reset} is a game made by {Colors.Bright_Blue}Fyuki Games{Colors.Reset}
-Fyuki Games is a netherlands-based one-man text-based game and project development studio;
-Owned by {Colors.Bright_Blue}MiataBoy{Colors.Reset}, and development being upheld entirely by him and the community.
+Fyuki Games is a netherlands-based text-based game and project development studio;
+Owned by {Colors.Bright_Blue}MiataBoy{Colors.Reset}, and development being upheld by the community and its developers..
 
 You can find all contributors here:
 {Colors.Bright_Blue}https://github.com/FyukiGames/delusionist/graphs/contributors
 
 {Colors.Reset}Organization links:
 {Colors.Bright_Blue}https://github.com/FyukiGames
-<Discord incoming soon>
+https://discord.gg/wf89Guxdjb
 
 {Colors.Reset}MiataBoy socials:
 {Colors.Bright_Blue}https://github.com/MiataBoy
